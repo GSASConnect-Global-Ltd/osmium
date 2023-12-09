42931435ad9a1e70a55bb8cb480e48ee780db70b
Alvin Okafor <alvin2k99@gmail.com>
2023-12-09 18:07:13 +0100

    ui modified

--- src/components/blog/BlogPost.tsx
+++ src/components/blog/BlogPost.tsx
@@ -30,7 +30,7 @@ interface Post {
         return (
             <div className="w-[384px] h-[523px] pt-[24px] pr-[24px] pb-[32px] pl-[24px] shadow-xl mr-4" key={post._id}>
                 <div className="">
-                    <img src={post.imageUrl} alt="" className="w-[336px] h-[240]" height={336} width={240} />
+                    <div className="h-[240px] w-[336px]"><img src={post.imageUrl} alt="" className="w-full h-full object-cover object-center" height={336} width={240} /></div>
                     <div className="mt-10">
                  <Link href={`/posts/${post._id}`}><p className="text-[#101828] text-2xl font-semibold"> {post.title}</p></Link>
                         <p className="text-[#667085] text-sm mt-4"></p>
